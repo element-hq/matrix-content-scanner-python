0555d20a1d3c0f81e3292216346cb16b6c350b30
Brendan Abolivier <babolivier@matrix.org>
2022-06-15 15:36:29 +0200

    Base code infrastructure for running the content scanner (#13)
    Main MatrixContentScanner class, config parsing, and HTTP server

---
+++ matrix_content_scanner/httpserver.py
@@ -0,0 +1,61 @@
+#  Copyright 2022 The Matrix.org Foundation C.I.C.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+import logging
+from typing import TYPE_CHECKING
+
+from twisted.web.resource import Resource
+from twisted.web.server import Site
+
+if TYPE_CHECKING:
+    from matrix_content_scanner.mcs import MatrixContentScanner
+
+logger = logging.getLogger(__name__)
+
+
+class HTTPServer:
+    def __init__(self, mcs: "MatrixContentScanner"):
+        self._mcs = mcs
+        self._bind_address = mcs.config.web.host
+        self._bind_port = mcs.config.web.port
+
+        root = self._build_resource_tree()
+        self._site = Site(root)
+
+    def _build_resource_tree(self) -> Resource:
+        """Creates a resource tree with all the servlets.
+
+        Returns:
+            The root resource with the servlets attached to it.
+        """
+        root = Resource()
+        matrix = Resource()
+        media_proxy = Resource()
+        unstable = Resource()
+
+        root.putChild(b"_matrix", matrix)
+        matrix.putChild(b"media_proxy", media_proxy)
+        media_proxy.putChild(b"unstable", unstable)
+
+        return root
+
+    def start(self) -> None:
+        """Starts the HTTP server."""
+        logger.info("Starting listener on %s:%d", self._bind_address, self._bind_port)
+
+        self._mcs.reactor.listenTCP(
+            interface=self._bind_address,
+            port=self._bind_port,
+            factory=self._site,
+            backlog=50,
+        )
