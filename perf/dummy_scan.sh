f21a88e8a6b44fe1f3b84fe80471be78364af34e
David Robertson <davidr@element.io>
2023-11-28 18:41:15 +0000

    Mock `dummy_scan.sh` to randomly fail

--- perf/dummy_scan.sh
+++ perf/dummy_scan.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
 sleep 1
+# Roughly a 5% change to fail, assuming a uniform distribution of $RANDOM.
+if [ $((RANDOM % 20)) = 0 ]; then
+  echo "I don't like the look of $1" > /dev/stderr
+  exit 1
+fi
